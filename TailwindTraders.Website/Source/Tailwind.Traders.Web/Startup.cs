ff22f9c3a7712cccf78a4e47938fabb5a71b4a69
J.-M. Laser <58994012+laserjm@users.noreply.github.com>
2022-04-12 21:16:25 +0200

    Update Startup.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-
+// trigger pr verification
 namespace Tailwind.Traders.Web
 {
     public class Startup
